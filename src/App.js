baf4f88269b3dd0c95e564176dbf0ca727016488
Alfonso BT <ponchobt.dev@gmail.com>
2023-04-18 22:59:54 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ChildComponent from "./ChildComponent";
+
+function ParentComponent() {
+  const greeting = "Hello, World!";
+
+  return (
+    <div>
+      <ChildComponent message={greeting} />
+    </div>
+  );
+}
+
+export default ParentComponent;
